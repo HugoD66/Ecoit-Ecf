f920a26b6e43ea2e6059bb15562e847915fc0ab0
Ecoit-Hugo DESSAUW <dessauw.hugo@gmail.com>
2022-04-08 00:20:38 +0200

    Test PostulerType/Controller/Entity

---
+++ src/Controller/PostulerController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Postuler;
+use App\Form\PostulerType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostulerController extends AbstractController
+{
+    #[Route('/postuler', name: 'app_postuler')]
+    public function new(Request $request): Response
+    {
+    $postuler = new Postuler();
+    $form = $this->createForm(PostulerType::class, $postuler);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $task = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('postuler/index.html.twig', [
+            'controller_name' => 'PostulerController',
+        ]);
+    }
+}
---
+++ src/Form/PostulerType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostulerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('roles')
+            ->add('password', PasswordType::class)
+            ->add('username', TextType::class)
+            ->add('lastname',TextType::class)
+            ->add('name',TextType::class)
+            ->add('description',TextType::class)
+            ->add('picture',TextType::class)
+            ->add('validate', CheckboxType::class)
+            ->add('isVerified', FileType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}

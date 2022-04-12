f740becb403ee80b8bec90e17a9e613aef79fb99
Ecoit-Hugo DESSAUW <dessauw.hugo@gmail.com>
2022-04-12 10:14:21 +0200

    Debug buttons footer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/teatcher.html.twig', [
+        return $this->render('home/index.html.twig', [
             'title' => 'Page d\'acceuil Eco IT'
         ]);
     }
